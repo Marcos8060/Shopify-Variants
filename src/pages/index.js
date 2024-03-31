de3fc7f6cbf0ab929e5c72266e93007fcf7170c9
Marcos8060 <marcosgav80@gmail.com>
2024-03-31 12:19:07 +0300

    mocked the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
-import CustomizedLayout from "@/components/layout/customized-layout";
+import Layout from "@/components/layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section>Welcome to the Home Page</section>
+    <section className="bg-gradient-to-r from-emerald-400 to-cyan-400 h-screen py-4">
+      <div>
+        <img className="w-2/12 mx-auto" src="/shopify.png" alt="" />
+      </div>
+      <section className="flex items-center justify-center h-[70vh] text-center w-6/12 mx-auto">
+        <div className="space-y-4">
+          <h1 className="font-bold text-5xl">
+            Bring your ideas to life for $1/month
+          </h1>
+          <p className="text-xl">The future of business is yours to shape.</p>
+          <div>
+            <Link href="/dashboard/product">
+              <button className="bg-black rounded-3xl text-white px-6 py-3 text-sm">
+                View Products
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </section>
   );
 }
-
-Home.getLayout = (page) => (
-  <CustomizedLayout>{page}</CustomizedLayout>
-)
